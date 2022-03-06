f2a6c4f2f80ec5705d519534bb94f99eb47b3bf4
Georgi <1229206@nvna.eu>
2022-03-06 18:02:43 +0200

    nedovursheno

---
+++ index.ts
@@ -0,0 +1,62 @@
+// Да създадем уеб сървър, който поддържа следните маршрути (routes).
+//     Чрез един маршрут искаме да можем да добавяме количество произволни числа към масив (който се складира в сървъра). Количеството
+// числа ще получим като query parameter.
+//     Чрез втори маршрут искаме да можем да намерим произволно число от масива и да го върнем като резултат ( ако конкретното число го няма - връщаме 0 ).
+// Чрез трети маршрут искаме да върнем като JSON резултат колко записа от всяко число има в масива:
+//     - За всяко число в масива, преброяваме колко други такива има и ги запазваме като стойност към ключ в JSON обект т.е за масив
+//     [2, 4, 2, 3, 4, 0, 1] ще получим като резултат
+// {
+//     0: 1, // една нула
+//     1: 1, // една единица
+//     2: 2 // две двойки
+//     3: 1, // една тройка
+//     4: 2 // две четворки
+// }
+
+
+import {response} from "express";
+
+const express = require("express")
+const port = 80;
+const app = express();
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`)
+})
+
+
+const randomNumber = (max) => {
+    return Math.floor(Math.random() * max)
+}
+
+const randomNumbers = (amount) => {
+    // const result = Array(amount).fill(0,0).map(e => randomNumber(10));
+    // return result;
+    let result = [];
+    for(let i = 0; i < amount; i++) {
+        result.push(randomNumber(10));
+    }
+
+    return result;
+}
+
+app.get("/", (request, response) => {
+    response.send("zdr kp");
+})
+
+app.get("/numbers", (req, res) => {
+    const params = req.query;
+    let numbers = randomNumbers(params.amount);
+    res.send(numbers);
+});
+
+app.get("/randomNumber",(req,res) =>{
+    const params= req.query;
+    let numbers =randomNumbers(params.amount);
+    let rNumber =Math.random();
+
+    res.send(numbers[rNumber]);
+
+});
+
+
